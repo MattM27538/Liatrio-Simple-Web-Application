3c732c683a2978fcc22f7696edd239b082279692
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-03-08 16:38:20 -0800

    Initial push app JSON files

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const app=express()
+const port=3000
+
+const myObject={"message":"My name is Matthew Martinez,","timestamp":Date.now()};
+
+//Stringifying object before writing it out to remove "pretty print" header.
+const jsonString=JSON.stringify(myObject);
+
+//Send object information in response to '/' PATH request.
+app.get('/',(req,res)=>{
+    res.send(jsonString)
+})
+
+//Have app listen on port 3000
+app.listen(port,()=>{
+    console.log(`App is listening on ${port}`)
+})
